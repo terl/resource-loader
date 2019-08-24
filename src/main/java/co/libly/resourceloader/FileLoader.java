1905921be771cecdc8746796dc2dbadb511283b7
Gurpreet Paul <gurpreet@gurpreet.co>
2019-08-24 16:00:26 +0100

    Commented FileLoader.java

--- src/main/java/co/libly/resourceloader/FileLoader.java
+++ src/main/java/co/libly/resourceloader/FileLoader.java
@@ -12,10 +12,26 @@ public class FileLoader extends ResourceLoader {
         super();
     }
 
+    /**
+     * Load a file/directory from your resource folder using a relative path.
+     * This will return your file or directory with global read, write and execute.
+     * @param relativePath Relative path to your file or directory.
+     * @return The file your directory.
+     * @throws IOException
+     */
     public File load(String relativePath) throws IOException {
         return load(relativePath, new HashSet<>());
     }
 
+    /**
+     * Load a file/directory from your resource folder with permissions
+     * you set. On windows, any type of read, write and execute permissions will
+     * be set appropriately.
+     * @param relativePath Relative path to your file or directory.
+     * @param permissions A set of permissions.
+     * @return The file your directory.
+     * @throws IOException
+     */
     public File load(String relativePath, Set<PosixFilePermission> permissions) throws IOException {
         return loadFromRelativePath(relativePath, permissions);
     }
