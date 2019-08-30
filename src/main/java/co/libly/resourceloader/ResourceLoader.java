a823174add746c79477a3c908004b94c36c00f5f
Gurpreet Paul <gurpreet@gurpreet.co>
2019-08-30 23:42:31 +0100

    Update resource loader to use official grabbing of JARs

--- src/main/java/co/libly/resourceloader/ResourceLoader.java
+++ src/main/java/co/libly/resourceloader/ResourceLoader.java
@@ -9,7 +9,10 @@
 package co.libly.resourceloader;
 
 
+import com.sun.jna.Platform;
+
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.channels.FileChannel;
@@ -98,7 +101,7 @@ public class ResourceLoader {
      * @throws IOException If whilst unzipping we had some problems.
      */
     private File getFileFromJar(URL jarUrl, File outputDir, String pathInJar) throws URISyntaxException, IOException {
-        File jar = new File(jarUrl.toURI());
+        File jar = urlToFile(jarUrl);
         unzip(jar.getAbsolutePath(), outputDir.getAbsolutePath());
         String filePath = outputDir.getAbsolutePath() + pathInJar;
         return new File(filePath);
@@ -359,11 +362,113 @@ public class ResourceLoader {
     }
 
     /**
-     * If we're in a JAR, we can get our path
-     * using this method.
-     * @return URL of this JAR.
+     * Gets the base location of the given class.
+     * From SciJava class.
+     * <p>
+     * If the class is directly on the file system (e.g.,
+     * "/path/to/my/package/MyClass.class") then it will return the base directory
+     * (e.g., "file:/path/to").
+     * </p>
+     * <p>
+     * If the class is within a JAR file (e.g.,
+     * "/path/to/my-jar.jar!/my/package/MyClass.class") then it will return the
+     * path to the JAR (e.g., "file:/path/to/my-jar.jar").
+     * </p>
+     *
+     * @param c The class whose location is desired.
      */
-    public URL getThisJarPath(Class outsideClass) {
-        return outsideClass.getProtectionDomain().getCodeSource().getLocation();
+    public static URL getThisJarPath(final Class<?> c) {
+        if (c == null) return null; // could not load the class
+
+        // try the easy way first
+        try {
+            final URL codeSourceLocation =
+                    c.getProtectionDomain().getCodeSource().getLocation();
+            if (codeSourceLocation != null) return codeSourceLocation;
+        }
+        catch (final SecurityException e) {
+            // NB: Cannot access protection domain.
+        }
+        catch (final NullPointerException e) {
+            // NB: Protection domain or code source is null.
+        }
+
+        // NB: The easy way failed, so we try the hard way. We ask for the class
+        // itself as a resource, then strip the class's path from the URL string,
+        // leaving the base path.
+
+        // get the class's raw resource path
+        final URL classResource = c.getResource(c.getSimpleName() + ".class");
+        if (classResource == null) return null; // cannot find class resource
+
+        final String url = classResource.toString();
+        final String suffix = c.getCanonicalName().replace('.', '/') + ".class";
+        if (!url.endsWith(suffix)) return null; // weird URL
+
+        // strip the class's path from the URL string
+        final String base = url.substring(0, url.length() - suffix.length());
+
+        String path = base;
+
+        // remove the "jar:" prefix and "!/" suffix, if present
+        if (path.startsWith("jar:")) path = path.substring(4, path.length() - 2);
+
+        try {
+            return new URL(path);
+        }
+        catch (final MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Converts the given {@link URL} to its corresponding {@link File}.
+     * From SciJava class.
+     * <p>
+     * This method is similar to calling {@code new File(url.toURI())} except that
+     * it also handles "jar:file:" URLs, returning the path to the JAR file.
+     * </p>
+     *
+     * @param url The URL to convert.
+     * @return A file path suitable for use with e.g. {@link FileInputStream}
+     * @throws IllegalArgumentException if the URL does not correspond to a file.
+     */
+    public static File urlToFile(final URL url) {
+        return url == null ? null : urlToFile(url.toString());
+    }
+
+    /**
+     * Converts the given URL string to its corresponding {@link File}.
+     * From SciJava class.
+     * @param url The URL to convert.
+     * @return A file path suitable for use with e.g. {@link FileInputStream}
+     * @throws IllegalArgumentException if the URL does not correspond to a file.
+     */
+    public static File urlToFile(final String url) {
+        String path = url;
+        if (path.startsWith("jar:")) {
+            // remove "jar:" prefix and "!/" suffix
+            final int index = path.indexOf("!/");
+            path = path.substring(4, index);
+        }
+        try {
+            if (Platform.isWindows() && path.matches("file:[A-Za-z]:.*")) {
+                path = "file:/" + path.substring(5);
+            }
+            return new File(new URL(path).toURI());
+        }
+        catch (final MalformedURLException e) {
+            // NB: URL is not completely well-formed.
+        }
+        catch (final URISyntaxException e) {
+            // NB: URL is not completely well-formed.
+        }
+        if (path.startsWith("file:")) {
+            // pass through the URL as-is, minus "file:" prefix
+            path = path.substring(5);
+            return new File(path);
+        }
+        throw new IllegalArgumentException("Invalid URL: " + url);
     }
 }
